6d37d2f409d259678afe17dbd64194962fe06464
jhoacar <carrerojhoan@gmail.com>
2022-06-12 18:08:40 -0300

    added concurrently

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,10 @@
-import server from './bootstrap/server';
-import dynamicImport from './bootstrap/database';
+import server from './boot/server';
+import dynamicImport from './boot/database';
 
 const port = process.env.PORT || 4000;
 
 server.listen(port, async () => {
-    console.log(`Server listening on port ${port}`);
+    console.log(`Server listening on http://localhost:${port}`);
     const { default: initalization } = await dynamicImport;
     initalization();
 })
--- server/src/routes/web/index.ts
+++ server/src/routes/web/index.ts
@@ -1,12 +1,14 @@
-import { Router, static as ShareStatic } from "express";
+import { Request, Response, Router, static as ShareStatic } from "express";
 import path from "path";
 
 const router = Router();
 
 const clientFolder = path.resolve(__dirname + '/../../../../client/build');
 
-router.use("/**", ShareStatic(clientFolder, {
-    cacheControl: false
-}));
+router.use("/", ShareStatic(clientFolder));
+
+router.use("/*", (req: Request, res: Response) => {
+    return res.sendFile(path.resolve(clientFolder + '/index.html'));
+})
 
 export default router;
